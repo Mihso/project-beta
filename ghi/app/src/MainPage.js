05849d799070cf65c491f0823c998a052b54b0df
Matthew Oshimo <moshimo90696@gmail.com>
2022-08-03 14:08:41 -0700

    slight change

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,7 +1,7 @@
 function MainPage() {
   return (
   <div className="container">
-    <div className="px-4 py-5 my-5 rounded-pill text-center bg-success">
+    <div className="px-4 py-5 my-5 mt-2 rounded-pill text-center bg-success">
       <h1 className="display-5 fw-bold">CarCar</h1>
     </div>
       <div className="col-lg-6 mx-auto">
