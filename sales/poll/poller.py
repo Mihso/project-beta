4dfb95f176cbd2f8b5bf8a6d33acd5f4a37b884e
Matthew Oshimo <moshimo90696@gmail.com>
2022-08-03 10:57:53 -0700

    small adjustments to poller

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -5,7 +5,7 @@ import time
 import json
 import requests
 
-sys.path.append(os.path.abspath(''))
+sys.path.append('')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_project.settings")
 django.setup()
 
